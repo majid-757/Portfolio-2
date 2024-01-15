ae68d8bdaddec65666b5d898e80e33c8560fb968
unknown <majid.asadollahi95@gmail.com>
2024-01-15 12:37:02 +0330

    clean code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,6 @@
-import { AiFillGithub } from "react-icons/ai"
-import { FaLinkedinIn, FaInstagram, FaTelegram } from "react-icons/fa"
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn, FaInstagram, FaTelegram } from "react-icons/fa";
 // import { FiTwitter } from "react-icons/fi"
-
 
 const Banner = () => {
   return (
@@ -17,22 +16,34 @@ const Banner = () => {
           delectus ipsam commodi.
         </p>
         <div className="flex mt-8 gap-2">
-            <div className="flex items-center justify-center">
-                <div className="flex space-x-2">
-                    <a href="" className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2">
-                        <AiFillGithub className="text-[28px]" />
-                    </a>
-                    <a href="" className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2">
-                        <FaLinkedinIn className="text-[28px]" />
-                    </a>
-                    <a href="" className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2">
-                        <FaTelegram className="text-[28px]" />
-                    </a>
-                    <a href="" className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2">
-                        <FaInstagram className="text-[28px]" />
-                    </a>
-                </div>
+          <div className="flex items-center justify-center">
+            <div className="flex space-x-2">
+              <a
+                href=""
+                className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
+              >
+                <AiFillGithub className="text-[28px]" />
+              </a>
+              <a
+                href=""
+                className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
+              >
+                <FaLinkedinIn className="text-[28px]" />
+              </a>
+              <a
+                href=""
+                className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
+              >
+                <FaTelegram className="text-[28px]" />
+              </a>
+              <a
+                href=""
+                className="text-fuchsia-600 hover:text-fuchsia-500 rounded-full glow p-2"
+              >
+                <FaInstagram className="text-[28px]" />
+              </a>
             </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-scroll";
-import { FaTimes } from "react-icons/fa"
-import { CiMenuFries } from "react-icons/ci"
-
+import { FaTimes } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
 const Nav = () => {
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click)
-    
+  const handleClick = () => setClick(!click);
 
   const content = (
     <>
@@ -53,31 +51,38 @@ const Nav = () => {
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18px]">
               <Link spy={true} smooth={true} to="Home">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Home
+                </li>
               </Link>
               <Link spy={true} smooth={true} to="About">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  About
+                </li>
               </Link>
               <Link spy={true} smooth={true} to="Services">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Services</li>
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Services
+                </li>
               </Link>
               <Link spy={true} smooth={true} to="Projects">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Projects</li>
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Projects
+                </li>
               </Link>
               <Link spy={true} smooth={true} to="Contact">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
+                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                  Contact
+                </li>
               </Link>
             </ul>
           </div>
         </div>
-        <div>
-          { click && content }
-        </div>
+        <div>{click && content}</div>
 
         <button className="block sm:hidden transition" onClick={handleClick}>
           {click ? <FaTimes /> : <CiMenuFries />}
         </button>
-
       </div>
     </nav>
   );
