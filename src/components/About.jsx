dcdabc94c560c8fd7ecfa39ffc4474b27ba36904
unknown <majid.asadollahi95@gmail.com>
2024-01-16 22:08:46 +0330

    Change some parts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,29 @@
-
-
 import img from "../assets/62511773.jpg";
 
 const About = () => {
   return (
-    <div id="About" className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col justify-between lg:gap-28 items-center">
-      <img data-aos="fade-down"
+    <div
+      id="About"
+      className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col justify-between lg:gap-28 items-center"
+    >
+      <img
+        data-aos="fade-down"
         src={img}
         width={290}
         height={290}
         className="rounded border-2 p-1 border-fuchsia-500 img-glow"
       />
       <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-        <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">
+        <h1
+          data-aos="fade-right"
+          className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase"
+        >
           About me
         </h1>
         <p data-aos="fade-left">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-          blanditiis eos vitae dolore aliquid sapiente cumque fuga recusandae
-          soluta enim. Nam iure animi quaerat recusandae repellendus culpa
-          delectus ipsam commodi.
+          Interested in software analysis and development, eager to learn new
+          technologies and experience new challenges, fast learner To complete
+          tasks independently and jointly.
         </p>
         <div className="flex mt-8 gap-2">
           <div className="flex items-center justify-center">
